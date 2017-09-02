381f943a18d01da51b40497482b83633d5a63e1b
Anand <anand.pal2050@gmail.com>
2017-09-02 00:38:27 -0800

    initial commit

--- _site/js/main.js
+++ _site/js/main.js
@@ -7,3 +7,25 @@ $(window).on('load',function(){
  $(".wrapper").show();
 
 });
+
+// carousel
+$('.owl-carousel').owlCarousel({
+
+   loop:true,
+   margin:7,
+   autoplay:true,
+   autoplayTimeout:2000,
+   autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+
+    }
+})
