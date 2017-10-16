67777d1c26ee4f3ef2a93ad1b1d3a6960ddb570f
jekyllx <anand.pal2050@gmail.com>
2017-10-16 22:47:50 +0530

    commit

--- _site/js/main.js
+++ _site/js/main.js
@@ -33,3 +33,17 @@ $('.owl-carousel').owlCarousel({
 $.validate({
     lang: 'en'
   });
+
+
+// when search icon is clicked
+$('#search').click(function () {
+  $('#search-container input').css('display','inherit');
+});
+
+
+// Jekyll search
+SimpleJekyllSearch({
+  searchInput: document.getElementById('search-input'),
+  resultsContainer: document.getElementById('results-container'),
+  json: '/search.json'
+})
