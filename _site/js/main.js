e7ffba8f2cba9767eee521582318d85cd2def8a4
jekyllx <anand.pal2050@gmail.com>
2017-11-08 10:52:08 +0530

    initial commit

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,30 +1,30 @@
 //video player manipulation
 $(".stream-options a").click(function(event) {
-  event.preventDefault();
-  $getData = $(this).attr("href");
-  document.getElementById("my-video_html5_api").src = $getData;
-  document.getElementById("my-video_html5_api").load();
-  document.getElementById("my-video_html5_api").play();
-  $(window).scrollTop(0);
+    event.preventDefault();
+    $getData = $(this).attr("href");
+    document.getElementById("my-video_html5_api").src = $getData;
+    document.getElementById("my-video_html5_api").load();
+    document.getElementById("my-video_html5_api").play();
+    $(window).scrollTop(0);
 });
 
 // carousel
 $('.owl-carousel').owlCarousel({
 
-   loop:true,
-   margin:8,
-   autoplay:true,
-   autoplayTimeout:2000,
-   autoplayHoverPause:false,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 8,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: false,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:3
+        600: {
+            items: 3
         },
-        1000:{
-            items:4
+        1000: {
+            items: 4
         }
     }
 });
@@ -32,18 +32,18 @@ $('.owl-carousel').owlCarousel({
 // form validation
 $.validate({
     lang: 'en'
-  });
+});
 
 
 // when search icon is clicked
-$('#search').click(function () {
-  $('#search-container input').css('display','inherit');
+$('#search').click(function() {
+    $('#search-container input').css('display', 'inherit');
 });
 
 
 // Jekyll search
 SimpleJekyllSearch({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
-  json: '/search.json'
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: '/search.json'
 })
